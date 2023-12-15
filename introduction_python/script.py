d4953b0b8fe987a200715dc97f94f827cccf129a
aminata <caminatamagath@gamil.com>
2023-12-15 16:21:33 +0100

    dgdfgd

---
+++ introduction_python/script.py
@@ -0,0 +1,19 @@
+blague = input("Si repete et pepete  sont dans un bateau, pepete tombe à l'eau, qu'est ce qui reste?\n")
+
+print(blague)
+resultat = input()
+
+while resultat != "tu es lourd":
+    if resultat =="repete":
+     resultat = input("Si repete et pepete  sont dans un bateau, pepete tombe à l'eau, qu'est ce qui reste?\n")
+    else:
+
+       resultat =  input("Mais non tu comprends pas, si repete et pepete  sont dans un bateau, pepete tombe à l'eau, qu'est ce qui reste ")
+    
+
+    print("le jeu est fini")
+
+   
+
+ 
+ 
