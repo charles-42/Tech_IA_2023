36a9ba14f08daa15d1e2521a9c85ea8725318f3a
Djovan Chathuant <d.chathuant@hotmail.com>
2023-12-15 11:19:36 +0100

    Work saved

---
+++ introduction_python/script.py
@@ -0,0 +1,17 @@
+print("Hello World")
+
+print ("This is not my first program")
+
+
+
+
+# intégrer ce lien quand on clique sur Zen of Python
+
+
+# Python core philosphy
+# Its core philosophy is summarized in the document The Zen of Python (PEP 20), which includes aphorisms such as:[72]
+# Beautiful is better than ugly.
+# Explicit is better than implicit.
+# Simple is better than complex.
+# Complex is better than complicated.
+# Readability counts.
