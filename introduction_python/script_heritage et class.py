50dc0ba09675181450cad14b4a56f03e58dfee73
LaGuize <guilhem.dolet@gmail.com>
2023-12-27 09:36:09 +0100

    First commit  At school

---
+++ introduction_python/script_heritage et class.py
@@ -0,0 +1,14 @@
+def nombre_voiture_par_marque(self,marque):
+    marque += 1
+    nombre_voiture_par_marque = len(marque)
+    return nombre_voiture_par_marque
+
+
+def ajouter_une_voiture(self,marque):
+     = 1
+
+
+def retirer_une_voiture(self,marque):
+    marque = 1
+    retirer_ = marque.pop(Voiture)
+    return retirer_
