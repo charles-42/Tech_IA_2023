867437ab8b70baaf8ca207a313e8d64afbb30f7d
LaGuize <guilhem.dolet@gmail.com>
2023-12-27 09:49:14 +0100

    Commit on script heritage

--- introduction_python/script_heritage et class.py
+++ introduction_python/script_heritage et class.py
@@ -1,11 +1,15 @@
 def nombre_voiture_par_marque(self,marque):
-    marque += 1
-    nombre_voiture_par_marque = len(marque)
-    return nombre_voiture_par_marque
+    nombre_voiture_marque_en_inventaire = 0
+    for voiture in self.inventaire:
+        if voiture.marque == marque_recherchee:
+            nombre_voiture_marque_en_inventaire += 1
 
 
-def ajouter_une_voiture(self,marque):
-     = 1
+def ajouter_une_voiture(self,voiture):
+    isinstance(voiture,Voiture)
+    for voiture in self.inventaire:
+        ajouter_une_voiture = inventaire.append(voiture)
+     return ajouter_une_voiture
 
 
 def retirer_une_voiture(self,marque):
